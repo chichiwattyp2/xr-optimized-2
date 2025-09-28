b3f53ec3424414dd6be455369ad0ef96d2f3bd4c
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-09-28 12:59:02 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,34 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  root: '.',  // root is the project root
-  publicDir: 'assets',  // or wherever your static assets live
+  root: '.',
+  publicDir: 'public',
   build: {
-    outDir: 'dist',  // your build output folder
+    outDir: 'dist',
     rollupOptions: {
       input: {
-        main: './index.html',
-        // If you have multiple HTML entry points, list them:
-        // ar: './ar-showcase.html',
-        // gallery: './gallery.html',
+        index: resolve(__dirname, 'index.html'),
+        xrtest: resolve(__dirname, 'XR-Test.html'),
+        arshowcase: resolve(__dirname, 'ar-showcase.html'),
+        assistantshop: resolve(__dirname, 'assistant-shop.html'),
+        assistant: resolve(__dirname, 'assistant.html'),
+        assistant1: resolve(__dirname, 'assistant1.html'),
+        beta: resolve(__dirname, 'beta.html'),
+        browser: resolve(__dirname, 'browser.html'),
+        callback: resolve(__dirname, 'callback.html'),
+        gallery: resolve(__dirname, 'gallery.html'),
+        integrated: resolve(__dirname, 'integrated_xr_experience.html'),
+        maps: resolve(__dirname, 'maps.html'),
+        material: resolve(__dirname, 'material_workspace.html'),
+        menu: resolve(__dirname, 'menu.html'),
+        oldchat: resolve(__dirname, 'old-chat.html'),
+        shop: resolve(__dirname, 'shop.html'),
+        siteindex: resolve(__dirname, 'site-index.html'),
+        terrain: resolve(__dirname, 'terrain.html'),
+        videoplayer: resolve(__dirname, 'video-player.html'),
+        vologram: resolve(__dirname, 'vologram.html'),
+        workspace: resolve(__dirname, 'workspace.html')
       }
     }
   }
