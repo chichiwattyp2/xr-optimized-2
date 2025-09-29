d49c9b7c77ec8bb73a5b228c973cc876d0508013
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-09-30 01:56:13 +0200

    Update page.jsx

--- src/app/(protected)/arPage/page.jsx
+++ src/app/(protected)/arPage/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 import dynamic from 'next/dynamic';
 const SceneComponent = dynamic(
-  () => import('@/components/common/sceneComponent.jsx'),
+  () => import('/components/common/sceneComponent.jsx'),
   { ssr: false }
 );
 
