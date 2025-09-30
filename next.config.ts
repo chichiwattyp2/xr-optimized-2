e98be3385ae97a4d9de94773ab8c74069e2e2753
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-09-30 02:37:10 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // ⚠️ This allows production builds to successfully complete
+    // even if there are ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
