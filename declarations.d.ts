6943768c53e2b50a6d96beb87f3e499f744291ac
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-09-30 07:22:37 +0200

    Delete declarations.d.ts

--- declarations.d.ts
+++
@@ -1,8 +0,0 @@
-// declarations.d.ts
-
-// Allow importing of .jsx and .js files in a TS project
-declare module "*.jsx";
-declare module "*.js";
-
-// Allow Next.js dynamic imports from JSX files
-declare module "@/components/*";
