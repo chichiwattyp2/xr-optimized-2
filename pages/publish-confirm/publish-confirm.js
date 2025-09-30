c1b7321baeda55a79b2e129e7b128b1c4ac81be7
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-09-30 09:28:56 +0200

    Update publish-confirm.js

--- pages/publish-confirm/publish-confirm.js
+++ pages/publish-confirm/publish-confirm.js
@@ -1,7 +1,8 @@
 "use client"
 
 import React, { useEffect } from "react"
-import { publishToGitHub } from "../../lib/packageUtils"
+import { publishToGitHub } from "@/lib/packageUtils"
+
 
 export default function PublishConfirmPage() {
   useEffect(() => {
