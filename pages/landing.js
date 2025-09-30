5fb926edc6e4f1a37056c5f0ca6d2a21c0dc051b
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-09-30 13:22:39 +0200

    Update landing.js

--- pages/landing.js
+++ pages/landing.js
@@ -6,9 +6,8 @@ export default function HomePage() {
   return (
     <>
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>XR Optimized – by Chris Watt-Pringle</title>
+ 
+  <title> XR Optimized by Chris Watt-Pringle </title>
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Orbitron:wght@500;700&display=swap" rel="stylesheet" />
