c3c4d5713ec403d2fe7f2c78f9ab1b1666e89941
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-09-30 16:08:27 +0200

    Update webar.js

--- pages/webar.js
+++ pages/webar.js
@@ -10,7 +10,7 @@ export default function WebARPage() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         {/* Styles */}
-        <link rel="stylesheet" href="/css/test.css" />
+        <link rel="stylesheet" href="css/test.css" />
 
         {/* A-Frame + AR.js */}
         <script src="https://aframe.io/releases/1.7.0/aframe.min.js"></script>
