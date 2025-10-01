5be93af588bb497f069a99e3dd25be2723649680
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-10-01 10:02:59 +0200

    Update pwa.js

--- public/js/pwa.js
+++ public/js/pwa.js
@@ -1,8 +1,8 @@
 'use strict';
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/xr-shop/service-worker.js',
-    {scope: '/xr-shop/'}
+  navigator.serviceWorker.register('service-worker.js',
+    {scope: '/public/'}
   )
   .then(function(registration) {
     console.log('Registration successful, scope is:', registration.scope);
@@ -11,3 +11,4 @@ if ('serviceWorker' in navigator) {
     console.log('Service worker registration failed, error:', error);
   });
 }
+
