4f0638fefdeb8bed11a23198c2b3bd0d4d7b504a
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-10-02 04:35:57 +0200

    Update main.js

--- public/main/main.js
+++ public/main/main.js
@@ -1,55 +1,25 @@
-"use client"
+window.onload = () => {
+    [...document.querySelectorAll('input')].forEach((input) => input.checked = false);
+};
 
-import React, { useEffect, useState } from "react"
+/* Update start building anchor href location based on radioState */
+function radioOnclick(self) {
+    const radioState = self.getAttribute('id')
+    let startBuilding = document.getElementById("start-building")
 
-export default function MainPage() {
-  const [radioState, setRadioState] = useState(null)
-  const [errorVisible, setErrorVisible] = useState(false)
-
-  useEffect(() => {
-    if (typeof document !== "undefined") {
-      document.querySelectorAll("input").forEach((input) => {
-        input.checked = false
-      })
+    if (radioState == "marker") {
+        startBuilding.href = "pages/marker/index.html"
+    } else if (radioState == "location") {
+        startBuilding.href = "pages/location/index.html"
+    } else {
+        // Display error message
     }
-  }, [])
+}
 
-  const handleRadioClick = (e) => {
-    setRadioState(e.target.id)
-    setErrorVisible(false)
-  }
-
-  const handleAnchorClick = (e) => {
-    if (!radioState) {
-      e.preventDefault()
-      setErrorVisible(true)
+function anchorOnclick(self) {
+    const href = self.getAttribute('href')
+    if (!href) {
+        const error = document.getElementById("error")
+        error.style.visibility = "visible"
     }
-  }
-
-  const getHref = () => {
-    if (radioState === "marker") return "/marker"
-    if (radioState === "location") return "/location"
-    return ""
-  }
-
-  return (
-    <div>
-      <h1>Select AR Mode</h1>
-
-      <label>
-        <input type="radio" id="marker" name="mode" onClick={handleRadioClick} /> Marker
-      </label>
-      <label>
-        <input type="radio" id="location" name="mode" onClick={handleRadioClick} /> Location
-      </label>
-
-      <br />
-
-      <a id="start-building" href={getHref()} onClick={handleAnchorClick}>
-        Start Building
-      </a>
-
-      {errorVisible && <p style={{ color: "red" }}>Please select an option first.</p>}
-    </div>
-  )
 }
