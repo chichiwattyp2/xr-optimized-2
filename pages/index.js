d63896537761eeb3054812f27e119a5712af5972
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-10-05 12:55:52 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Landing() {
 
   const pages = [
     { name: "test", path: "/menutest.html" },
+    { name: "music player", path: "/aframe-webxr-synth.html" },
     { name: "components", path: "/components.html" },
      { name: "browser", path: "/xrbrowser.html" },
     { name: "Assistant1", path: "/assistant1.html" },
