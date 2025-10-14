c7cb0e097071730dcfe234136f6dfd37383932a5
Chris Watt-Pringle <53357288+chichiwattyp2@users.noreply.github.com>
2025-10-14 13:12:00 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Landing() {
   const [filter, setFilter] = useState("");
 
   const pages = [
+    { name: "AR/AI Spacial Chat", path: "/ar-ai-spacial-chat.html" },
     { name: "test", path: "/menutest.html" },
      { name: "Training", path: "/ilabs-training.html" },
     { name: "music player", path: "/aframe-webxr-synth.html" },
